e0c87a914933556dbf9fa000c4f392aeb90de3af
is386 <singh.indervir89@gmail.com>
2022-01-08 12:29:46 -0500

    add drawing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/is386/CHIP82
+
+go 1.15
+
+require (
+	github.com/faiface/pixel v0.10.0
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+)
--- main.go
+++ main.go
@@ -2,6 +2,35 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"image/color"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+var (
+	FILENAME = "ibm.ch8"
+	SCALE    = 10.0
+	FONT     = [80]byte{
+		0xF0, 0x90, 0x90, 0x90, 0xF0,
+		0x20, 0x60, 0x20, 0x20, 0x70,
+		0xF0, 0x10, 0xF0, 0x80, 0xF0,
+		0xF0, 0x10, 0xF0, 0x10, 0xF0,
+		0x90, 0x90, 0xF0, 0x10, 0x10,
+		0xF0, 0x80, 0xF0, 0x10, 0xF0,
+		0xF0, 0x80, 0xF0, 0x90, 0xF0,
+		0xF0, 0x10, 0x20, 0x40, 0x40,
+		0xF0, 0x90, 0xF0, 0x90, 0xF0,
+		0xF0, 0x90, 0xF0, 0x10, 0xF0,
+		0xF0, 0x90, 0xF0, 0x90, 0x90,
+		0xE0, 0x90, 0xE0, 0x90, 0xE0,
+		0xF0, 0x80, 0x80, 0x80, 0xF0,
+		0xE0, 0x90, 0x90, 0x90, 0xE0,
+		0xF0, 0x80, 0xF0, 0x80, 0xF0,
+		0xF0, 0x80, 0xF0, 0x80, 0x80}
 )
 
 type Stack []byte
@@ -20,5 +49,69 @@ func (s *Stack) pop() (byte, error) {
 	return val, nil
 }
 
+type Screen struct {
+	width, height, scale float64
+	window               *pixelgl.Window
+	drawer               *imdraw.IMDraw
+}
+
+func newScreen(scale float64) *Screen {
+	cfg := pixelgl.WindowConfig{
+		Title:  "CHIP-8 Emulator",
+		Bounds: pixel.R(0, 0, 64*scale, 32*scale),
+		VSync:  true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	imd := imdraw.New(nil)
+	screen := Screen{width: 64, height: 32, scale: SCALE, window: win, drawer: imd}
+	screen.Clear()
+	screen.Update()
+	return &screen
+}
+
+func (s *Screen) Clear() {
+	s.window.Clear(colornames.Black)
+}
+
+func (s *Screen) Update() {
+	s.window.Update()
+}
+
+func (s *Screen) Closed() bool {
+	return s.window.Closed()
+}
+
+func (s *Screen) DrawPixel(x0 float64, y0 float64, color color.RGBA) {
+	x := x0 * s.scale
+	y := y0 * s.scale
+	s.drawer.Color = color
+	s.drawer.Push(pixel.V(x, y))
+	s.drawer.Push(pixel.V(x+s.scale, y+s.scale))
+	s.drawer.Rectangle(0)
+	s.drawer.Draw(s.window)
+}
+
+func (s *Screen) GetColor(x float64, y float64) int {
+	color := s.window.Color(pixel.V(x*s.scale, y*s.scale))
+	fmt.Println(color)
+	if color.R == 0 && color.G == 0 && color.B == 0 && color.A == 1 {
+		return 0
+	}
+	return 1
+}
+
+func run() {
+	screen := newScreen(SCALE)
+	for !screen.Closed() {
+	}
+}
+
 func main() {
+	fmt.Println(FILENAME)
+	pixelgl.Run(run)
 }
