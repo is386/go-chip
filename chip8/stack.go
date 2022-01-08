6e269bda3f7229a7cf6c7a080613b983b85e20b8
is386 <singh.indervir89@gmail.com>
2022-01-08 17:28:11 -0500

    change stack from byte to uint16

--- chip8/stack.go
+++ chip8/stack.go
@@ -2,13 +2,13 @@ package chip8
 
 import "errors"
 
-type Stack []byte
+type Stack []uint16
 
-func (s *Stack) Push(val byte) {
+func (s *Stack) Push(val uint16) {
 	*s = append(*s, val)
 }
 
-func (s *Stack) Pop() (byte, error) {
+func (s *Stack) Pop() (uint16, error) {
 	if len(*s) == 0 {
 		return 0x0, errors.New("cannot pop from empty stack")
 	}
