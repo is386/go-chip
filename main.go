ca7e125443ccbd6554436b30a74fba0feeb04e06
is386 <singh.indervir89@gmail.com>
2022-01-09 16:03:45 -0500

    add file name arg

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/is386/GoCHIP/chip8"
@@ -8,11 +10,18 @@ import (
 )
 
 var (
-	FILENAME           = "roms/asd.ch8"
 	SCALE        int32 = 10
 	EMU_DELAY          = 2 * time.Millisecond
 	TICKER_DELAY       = 16 * time.Millisecond
 )
+
+func parseRomFile() string {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: main <rom_file>")
+		os.Exit(1)
+	}
+	return os.Args[1]
+}
 
 func emulatorTicker(emu *chip8.Emulator, ticker *time.Ticker, stopTicking chan bool) {
 	for {
@@ -26,6 +35,8 @@ func emulatorTicker(emu *chip8.Emulator, ticker *time.Ticker, stopTicking chan b
 }
 
 func main() {
+	rom := parseRomFile()
+
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
@@ -34,7 +45,7 @@ func main() {
 	screen := chip8.NewScreen(SCALE)
 	keypad := chip8.NewKeypad()
 	emu := chip8.NewEmulator(screen, keypad)
-	emu.LoadRom(FILENAME)
+	emu.LoadRom(rom)
 
 	ticker := time.NewTicker(TICKER_DELAY)
 	stopTicking := make(chan bool)
